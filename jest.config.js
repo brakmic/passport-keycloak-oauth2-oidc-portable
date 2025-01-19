252a3d4ddfabbec1163d13048302b5262c440c21
Harris Brakmic <brakmic@gmail.com>
2025-01-19 21:45:35 +0100

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,25 @@
 module.exports = {
-  preset: "ts-jest/presets/default", // Use ts-jest preset for TypeScript and Jest integration
-  testEnvironment: "node", // Use Node.js environment for testing
-  roots: ["<rootDir>/test"], // Test folder location
-  moduleFileExtensions: ["ts", "js"], // Allow both .ts and .js in tests
+  preset: "ts-jest/presets/default",
+  testEnvironment: "node",
+  roots: ["<rootDir>/test"],
+  moduleFileExtensions: ["ts", "js"],
   transform: {
     "^.+\\.ts$": [
-      "ts-jest", 
+      "ts-jest",
       {
-        tsconfig: "<rootDir>/tsconfig.test.json", // Use the test-specific tsconfig
+        tsconfig: "<rootDir>/tsconfig.test.json",
       },
-    ], // Transpile .ts files with ts-jest
+    ],
   },
-  testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore node_modules and dist during tests
-  collectCoverage: true, // Enable coverage collection
-  coverageDirectory: "<rootDir>/coverage", // Output directory for coverage reports
-  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore coverage for specific paths
-  verbose: true, // Display individual test results with execution times
+  testMatch: [
+    "**/test/unit/**/*.test.ts",
+    "**/test/integration/**/*.test.ts",
+    "**/test/e2e/**/*.test.ts",
+    "**/test/package/**/*.test.ts",
+  ],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  coveragePathIgnorePatterns: ["/node_modules/", "/dist/"],
+  verbose: true,
 };
